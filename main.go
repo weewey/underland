ee20aeaee608a61faf04012f520f02c75ef4fb6e
yewwee <ychua@thoughtworks.com>
2022-03-01 12:58:44 +0800

    Add dockerfile, main.go and procfile

--- main.go
+++ main.go
@@ -1,22 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/portto/solana-go-sdk/rpc"
 	"github.com/underland/clients"
 	"github.com/underland/handlers"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 
-	solanaClient := clients.NewSolanaClient(rpc.MainnetRPCEndpoint)
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	solanaClient := clients.NewSolanaClient(rpc.DevnetRPCEndpoint)
 
 	r := mux.NewRouter()
 	r.HandleFunc("/ping", handlers.HealthCheckHandler)
 	r.HandleFunc("/token-metadata", handlers.TokenMetaDataHandler(solanaClient)).Methods("GET")
 	r.HandleFunc("/increment-social-index", handlers.IncrementSocialIndexHandler(solanaClient)).Methods("POST")
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), r))
 }
